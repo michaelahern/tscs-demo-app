748a68e9f8d69d7bc240571154dd33c59d4d26be
Michael Ahern <michael.ahern@gmail.com>
2025-09-14 21:09:09 -0600

    RDS Aurora Serverless Test (#3)

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
 import { KMS } from '@aws-sdk/client-kms';
 import { S3 } from '@aws-sdk/client-s3';
+import { SecretsManager } from '@aws-sdk/client-secrets-manager';
 import { NodeHttpHandler } from '@smithy/node-http-handler';
-import { promises as dns } from 'node:dns';
+import { promises as DNS } from 'node:dns';
+import { Client as Postgres } from 'pg';
 
 const DYNAMODB_TABLE_NAME = 'tscs-demo-table-684209394034-us-east-2';
 const DYNAMODB_VPCE_HOSTNAME = 'vpce-05a34e75b50424ab0-lmwrdzf5.dynamodb.us-east-2.vpce.amazonaws.com';
 const KMS_KEY_ALIAS = 'tscs-demo-key-684209394034-us-east-2';
+const RDS_SECRET_NAME = 'AuroraClusterSecret8E4F2BC8-3XcGTtJyKDpt';
 const S3_BUCKET_NAME = 'tscs-demo-bucket-684209394034-us-east-2';
 
 async function testDynamoDB() {
     console.log('AWS DynamoDB [Interface Endpoint]');
 
     try {
-        const dnsResult = await dns.lookup(DYNAMODB_VPCE_HOSTNAME);
+        const dnsResult = await DNS.lookup(DYNAMODB_VPCE_HOSTNAME);
         console.log(`${DYNAMODB_VPCE_HOSTNAME}:`, dnsResult.address);
 
         const dynamodb = new DynamoDB({
@@ -53,7 +56,7 @@ async function testKMS() {
     console.log('AWS KMS [Interface Endpoint with VPC Private DNS]');
 
     try {
-        const dnsResult = await dns.lookup('kms.us-east-2.amazonaws.com');
+        const dnsResult = await DNS.lookup('kms.us-east-2.amazonaws.com');
         console.log(`kms.us-east-2.amazonaws.com:`, dnsResult.address);
 
         const kms = new KMS({
@@ -68,11 +71,40 @@ async function testKMS() {
     }
 }
 
+async function testRDS() {
+    console.log('AWS RDS Aurora Serverless v2 [Elastic Network Interface]');
+
+    try {
+        const secretsManager = new SecretsManager({ region: 'us-east-2' });
+        const secret = await secretsManager.getSecretValue({ SecretId: RDS_SECRET_NAME });
+        if (secret.SecretString === undefined) throw new Error('Database credentials not accessible...');
+        const credentials = JSON.parse(secret.SecretString);
+        console.log('Credentials:', credentials);
+
+        const client = new Postgres({
+            host: credentials.host,
+            port: credentials.port,
+            user: credentials.username,
+            password: credentials.password,
+            ssl: { rejectUnauthorized: false },
+            connectionTimeoutMillis: 5000
+        });
+
+        await client.connect();
+        const result = await client.query('SELECT version()');
+        console.log('SELECT version():', result.rows[0].version);
+        await client.end();
+    }
+    catch (err: unknown) {
+        console.error(err instanceof Error ? err.message : err);
+    }
+}
+
 async function testS3() {
     console.log('AWS S3 [Gateway Endpoint]');
 
     try {
-        const dnsResult = await dns.lookup('s3.us-east-2.amazonaws.com');
+        const dnsResult = await DNS.lookup('s3.us-east-2.amazonaws.com');
         console.log('s3.us-east-2.amazonaws.com:', dnsResult.address);
 
         const s3 = new S3({
@@ -97,6 +129,8 @@ async function main() {
     await testDynamoDB();
     console.log();
     await testKMS();
+    console.log();
+    await testRDS();
     console.log();
     await testS3();
 }
